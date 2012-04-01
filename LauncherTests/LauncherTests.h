0743c4aacdcc58b5b116666df1e9c4c8be91b467
Enrique W <Kique@Enrique-Ws-MacBook-Pro.local>
2012-03-31 21:47:23 -0400

    Initial Commit

---
+++ LauncherTests/LauncherTests.h
@@ -0,0 +1,13 @@
+//
+//  LauncherTests.h
+//  LauncherTests
+//
+//  Created by Enrique W on 3/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LauncherTests : SenTestCase
+
+@end
